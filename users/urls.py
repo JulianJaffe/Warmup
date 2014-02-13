5dd6c19fbeb1badf2e4c3419e4230b908c765ace
JulianJaffe <julianjaffe@berkeley.edu>
2014-02-13 07:12:12 -0800

    Fixed url
    (oops)

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
-    url(r'^adduser$', views.addUser, name='user'),
+    url(r'^add$', views.addUser, name='user'),
     url(r'^resetFixture$', views.resetFixture, name='reset'),
     url(r'^unitTests$', views.unitTests, name='test'),
 )
