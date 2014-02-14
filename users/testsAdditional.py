99086e5a9be1f1341f7801b75dc492773a0567d4
JulianJaffe <julianjaffe@berkeley.edu>
2014-02-14 15:48:57 -0800

    testAdditional.py cleanup
    Fingers crossed should be good to go (everything works locally)

--- users/testsAdditional.py
+++ users/testsAdditional.py
@@ -20,7 +20,7 @@ class TestLogin(unittest.TestCase):
         """Close the conection"""
         self.conn.close()
 
-    def makeRequest(self, url, data):
+    def makeRequest(self, url, data = {}):
         """Issues a POST request containing DATA to the specified URL.
         This code is adapted from the same method in testLib.py"""
         headers = {}
@@ -56,18 +56,18 @@ class TestLogin(unittest.TestCase):
     def testProper(self):
         """Test that the API properly handles correct behavior."""
         user1 = {'user': 'test1', 'password': 'swordfish'}
-        makeRequest("/users/add", data = user1)
-        response = makeRequest("/users/login", data = user1)
+        self.makeRequest("/users/add", data = user1)
+        response = self.makeRequest("/users/login", data = user1)
         self.assertResponse(response, count=2)
 
     def testBadCreds(self):
         """Test that the API properly handles incorrect behavior."""
         user2 = {'user': 'test2', 'password': ''}
-        makeRequest("/users/add", data = user2)
-        response = makeRequest("/users/login", data = {'user': 'test2', 'password': 'pass'})
-        self.assertResponse(response, errCode = ERR_BAD_CREDENTIALS)
-        response = makeRequest("/users/login", data = {'user': 'test3', 'password': ''})
-        self.assertResponse(response, errCode = ERR_BAD_CREDENTIALS)
+        self.makeRequest("/users/add", data = user2)
+        response = self.makeRequest("/users/login", data = {'user': 'test2', 'password': 'pass'})
+        self.assertResponse(response, errCode = self.ERR_BAD_CREDENTIALS)
+        response = self.makeRequest("/users/login", data = {'user': 'test3', 'password': ''})
+        self.assertResponse(response, errCode = self.ERR_BAD_CREDENTIALS)
 
 class TestResetFixture(unittest.TestCase):
     """Test the resetFixture behavior."""
@@ -87,7 +87,7 @@ class TestResetFixture(unittest.TestCase):
         """Close the conection"""
         self.conn.close()
 
-    def makeRequest(self, url, data):
+    def makeRequest(self, url, data = {}):
         """Issues a POST request containing DATA to the specified URL.
         This code is adapted from the same method in testLib.py"""
         headers = {}
@@ -123,46 +123,46 @@ class TestResetFixture(unittest.TestCase):
     def testResest(self):
         """Test the resetFixture API capability."""
         data = {'user': 'test3', 'password': 'pass1'}
-        makeRequest("/users/add", data = data)
-        makeRequest("/users/login", data = data)
-        response = makeRequest("/users/login", data = data)
+        self.makeRequest("/users/add", data = data)
+        self.makeRequest("/users/login", data = data)
+        response = self.makeRequest("/users/login", data = data)
         self.assertResponse(response, count=3)
-        response = makeRequest("/TESTAPI/resetFixture", data = {})
+        response = self.makeRequest("/TESTAPI/resetFixture", data = {})
         self.assertResponse(response)
-        response = makeRequest("/users/login", data = data)
-        self.assertResponse(response, errCode = ERR_BAD_CREDENTIALS) #test3 doesn't exist anymore
-        response = makeRequest("/users/add", data = data)
-        self.assertResponse(response, count=1)                                            #but it can be added
+        response = self.makeRequest("/users/login", data = data)
+        self.assertResponse(response, errCode=self.ERR_BAD_CREDENTIALS)     #test3 doesn't exist anymore
+        response = self.makeRequest("/users/add", data = data)
+        self.assertResponse(response, count=1)                              #but it can be added
 
     def testComprehensive(self):
         """Put it all together now."""
         user1 = {'user': 'user', 'password': ''}
-        response = makeRequest("/users/add", data = user1)          #Doesn't exist, so it can be created (even without a password)
+        response = self.makeRequest("/users/add", data = user1)          #Doesn't exist, so it can be created (even without a password)
         self.assertResponse(response, count=1)
-        response = makeRequest("/users/add", data = user1)          #But it can't be created again
-        self.assertResponse(response, TestCase.ERR_USER_EXISTS)
+        response = self.makeRequest("/users/add", data = user1)          #But it can't be created again
+        self.assertResponse(response, errCode=self.ERR_USER_EXISTS)
         user2 = {'user': 'test', 'password': '123456'}
-        response = makeRequest("/users/add", data = user2)          #A second user can be created though
+        response = self.makeRequest("/users/add", data = user2)          #A second user can be created though
         self.assertResponse(response, count=1)
-        response = makeRequest("/users/login", data = user2)        #And if the credentials match, you can log in
+        response = self.makeRequest("/users/login", data = user2)        #And if the credentials match, you can log in
         self.assertResponse(response, count=2)
         user3 = {'user': '', 'password': ''}
-        response = makeRequest("/users/add", data = user3)          #But you have to have a username
-        self.assertResponse(response, errCode=ERR_BAD_USERNAME)
+        response = self.makeRequest("/users/add", data = user3)          #But you have to have a username
+        self.assertResponse(response, errCode=self.ERR_BAD_USERNAME)
         user4 = {'user': 'John Q. Public', 'password': 'Sons of Liberty'}
-        response = makeRequest("/users/login", data = user4)        #And you have to exist before you can log in
-        self.assertResponse(response, errCode=ERR_BAD_CREDENTIALS)
-        response = makeRequest("/TESTAPI/resetFixture", data = {})  #And now the database is empty again
+        response = self.makeRequest("/users/login", data = user4)        #And you have to exist before you can log in
+        self.assertResponse(response, errCode=self.ERR_BAD_CREDENTIALS)
+        response = self.makeRequest("/TESTAPI/resetFixture", data = {})  #And now the database is empty again
         self.assertResponse(response)
-        response = makeRequest("/users/login", data = user2)        #Since the database is empty, we can't log in any more
-        self.assertResponse(response, errCode=ERR_BAD_CREDENTIALS)
-        response = makeRequest("/users/add", data = user4)          #But we can still add new users
+        response = self.makeRequest("/users/login", data = user2)        #Since the database is empty, we can't log in any more
+        self.assertResponse(response, errCode=self.ERR_BAD_CREDENTIALS)
+        response = self.makeRequest("/users/add", data = user4)          #But we can still add new users
         self.assertResponse(response, count=1)
-        response = makeRequest("/users/login", data = user4)        #And this time, we can log in
+        response = self.makeRequest("/users/login", data = user4)        #And this time, we can log in
         self.assertResponse(response, count=2)
-        response = makeRequest("/users/add", data = user2)          #Nothing stopping us from recreating the user
+        response = self.makeRequest("/users/add", data = user2)          #Nothing stopping us from recreating the user
         self.assertResponse(response, count=1)
-        response = makeRequest("/users/login", data = user2)        #Now we can log in again
+        response = self.makeRequest("/users/login", data = user2)        #Now we can log in again
         self.assertResponse(response, count=2)
         
         
@@ -185,7 +185,7 @@ class TestAddUser(unittest.TestCase):
         """Close the conection"""
         self.conn.close()
 
-    def makeRequest(self, url, data):
+    def makeRequest(self, url, data = {}):
         """Issues a POST request containing DATA to the specified URL.
         This code is adapted from the same method in testLib.py"""
         headers = {}
@@ -208,7 +208,7 @@ class TestAddUser(unittest.TestCase):
         except:
             raise
     
-    def assertResponse(self, respData, count = 1, errcode = SUCCESS):
+    def assertResponse(self, respData, count = 1, errCode = SUCCESS):
         """
         Check that the response data dictionary matches the expected values
         """
@@ -220,21 +220,21 @@ class TestAddUser(unittest.TestCase):
 
     def testAdd(self):
         """Simple user adding test"""
-        response = makeRequest("/users/add", data = {'user': 'user', 'password': 'password'})
+        response = self.makeRequest("/users/add", data = {'user': 'user', 'password': 'password'})
         self.assertResponse(response)
 
     def testDupeAdd(self):
         """Shouldn't be able to add the same user twice."""
-        makeRequest("/users/add", data = {'user' : 'user1', 'password' : 'password'})
-        response = makeRequest("/users/add", data = {'user' : 'user1', 'password' : 'password'})
-        self.assertResponse(response, count=None, errCode = ERR_USER_EXISTS)
+        self.makeRequest("/users/add", data = {'user' : 'user1', 'password' : 'password'})
+        response = self.makeRequest("/users/add", data = {'user' : 'user1', 'password' : 'password'})
+        self.assertResponse(response, count=None, errCode = self.ERR_USER_EXISTS)
 
     def testLong(self):
         """ Test length checking."""
         longstring = 129 * '*'
-        response = makeRequest("/users/add", data = {'user' : longstring, 'password' : 'password'})
-        self.assertResponse(response, count=None, errCode = ERR_BAD_USERNAME)
-        response = makeRequest("/users/add", data = {'user' : 'user', 'password' : longstring})
-        self.assertResponse(response, count=None, errCode = ERR_BAD_PASSWORD)
+        response = self.makeRequest("/users/add", data = {'user' : longstring, 'password' : 'password'})
+        self.assertResponse(response, count=None, errCode = self.ERR_BAD_USERNAME)
+        response = self.makeRequest("/users/add", data = {'user' : 'user', 'password' : longstring})
+        self.assertResponse(response, count=None, errCode = self.ERR_BAD_PASSWORD)
 
     
