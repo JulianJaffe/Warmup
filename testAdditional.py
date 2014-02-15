8b8067e99c4c8160712f16313d33fbca3e4da741
JulianJaffe <julianjaffe@berkeley.edu>
2014-02-14 16:18:38 -0800

    Rolled back testAdditional.py
    I totally misunderstood a Piazza posts, but thanks to the magic of
    version control, its all ok! testAdditional.py now depends on
    testLib.py, as before. It's also been moved to the root.

---
+++ testAdditional.py
@@ -0,0 +1,130 @@
+"""
+Each file that starts with test... in this directory is scanned for subclasses of unittest.TestCase or testLib.RestTestCase
+"""
+
+import unittest
+import os
+import testLib
+import httplib
+import json
+
+class TestLogin(testLib.RestTestCase):
+    """Test proper handling of logging in."""
+    def assertResponse(self, respData, count = None, errCode = testLib.RestTestCase.SUCCESS):
+        """
+        Check that the response data dictionary matches the expected values
+        """
+        #from testLib.py
+        expected = { 'errCode' : errCode }
+        if count is not None:
+            expected['count']  = count
+        self.assertDictEqual(expected, respData)
+    
+    def testProper(self):
+        """Test that the API properly handles correct behavior."""
+        user1 = {'user': 'test1', 'password': 'swordfish'}
+        self.makeRequest("/users/add", method="POST", data = user1)
+        response = self.makeRequest("/users/login", method="POST", data = user1)
+        self.assertResponse(response, count=2)
+
+    def testBadCreds(self):
+        """Test that the API properly handles incorrect behavior."""
+        user2 = {'user': 'test2', 'password': ''}
+        self.makeRequest("/users/add", method="POST", data = user2)
+        response = self.makeRequest("/users/login", method="POST", data = {'user': 'test2', 'password': 'pass'})
+        self.assertResponse(response, errCode = testLib.RestTestCase.ERR_BAD_CREDENTIALS)
+        response = self.makeRequest("/users/login", method="POST", data = {'user': 'test3', 'password': ''})
+        self.assertResponse(response, errCode = testLib.RestTestCase.ERR_BAD_CREDENTIALS)
+
+class TestResetFixture(testLib.RestTestCase):
+    """Test the resetFixture behavior."""
+    def assertResponse(self, respData, count = None, errCode = testLib.RestTestCase.SUCCESS):
+        """
+        Check that the response data dictionary matches the expected values
+        """
+        #from testLib.py
+        expected = { 'errCode' : errCode }
+        if count is not None:
+            expected['count']  = count
+        self.assertDictEqual(expected, respData)
+        
+    def testResest(self):
+        """Test the resetFixture API capability."""
+        data = {'user': 'test3', 'password': 'pass1'}
+        self.makeRequest("/users/add", method="POST", data = data)
+        self.makeRequest("/users/login", method="POST", data = data)
+        response = self.makeRequest("/users/login", method="POST", data = data)
+        self.assertResponse(response, count=3)
+        response = self.makeRequest("/TESTAPI/resetFixture", method="POST", data = {})
+        self.assertResponse(response)
+        response = self.makeRequest("/users/login", method="POST", data = data)
+        self.assertResponse(response, errCode = testLib.RestTestCase.ERR_BAD_CREDENTIALS) #test3 doesn't exist anymore
+        response = self.makeRequest("/users/add", method="POST", data = data)
+        self.assertResponse(response, count=1)                                            #but it can be added
+
+    def testComprehensive(self):
+        """Put it all together now."""
+        user1 = {'user': 'user', 'password': ''}
+        response = self.makeRequest("/users/add", method="POST", data = user1)          #Doesn't exist, so it can be created (even without a password)
+        self.assertResponse(response, count=1)
+        response = self.makeRequest("/users/add", method="POST", data = user1)          #But it can't be created again
+        self.assertResponse(response, errCode=testLib.RestTestCase.ERR_USER_EXISTS)
+        user2 = {'user': 'test', 'password': '123456'}
+        response = self.makeRequest("/users/add", method="POST", data = user2)          #A second user can be created though
+        self.assertResponse(response, count=1)
+        response = self.makeRequest("/users/login", method="POST", data = user2)        #And if the credentials match, you can log in
+        self.assertResponse(response, count=2)
+        user3 = {'user': '', 'password': ''}
+        response = self.makeRequest("/users/add", method="POST", data = user3)          #But you have to have a username
+        self.assertResponse(response, errCode=testLib.RestTestCase.ERR_BAD_USERNAME)
+        user4 = {'user': 'John Q. Public', 'password': 'Sons of Liberty'}
+        response = self.makeRequest("/users/login", method="POST", data = user4)        #And you have to exist before you can log in
+        self.assertResponse(response, errCode=testLib.RestTestCase.ERR_BAD_CREDENTIALS)
+        response = self.makeRequest("/TESTAPI/resetFixture", method="POST", data = {})  #And now the database is empty again
+        self.assertResponse(response)
+        response = self.makeRequest("/users/login", method="POST", data = user2)        #Since the database is empty, we can't log in any more
+        self.assertResponse(response, errCode=testLib.RestTestCase.ERR_BAD_CREDENTIALS)
+        response = self.makeRequest("/users/add", method="POST", data = user4)          #But we can still add new users
+        self.assertResponse(response, count=1)
+        response = self.makeRequest("/users/login", method="POST", data = user4)        #And this time, we can log in
+        self.assertResponse(response, count=2)
+        response = self.makeRequest("/users/add", method="POST", data = user2)          # Nothing stopping us from recreating the user
+        self.assertResponse(response, count=1)
+        response = self.makeRequest("/users/login", method="POST", data = user2)        #Now we can log in again
+        self.assertResponse(response, count=2)
+        
+        
+
+        
+class TestAddUser(testLib.RestTestCase):
+    """Test adding users"""
+    def assertResponse(self, respData, count = 1, errCode = testLib.RestTestCase.SUCCESS):
+        """
+        Check that the response data dictionary matches the expected values
+        """
+        #from testLib.py
+        expected = { 'errCode' : errCode }
+        if count is not None:
+            expected['count']  = count
+        self.assertDictEqual(expected, respData)
+
+    def testAdd(self):
+        """Simple user adding test"""
+        response = self.makeRequest("/users/add", method="Post", data = {'user': 'user', 'password': 'password'})
+        self.assertResponse(response)
+
+    def testDupeAdd(self):
+        """Shouldn't be able to add the same user twice."""
+        self.makeRequest("/users/add", method="POST", data = {'user' : 'user1', 'password' : 'password'})
+        response = self.makeRequest("/users/add", method="POST", data = {'user' : 'user1', 'password' : 'password'})
+        self.assertResponse(response, count=None, errCode = testLib.RestTestCase.ERR_USER_EXISTS)
+
+    def testLong(self):
+        """ Test length checking."""
+        longstring = 129 * '*'
+        response = self.makeRequest("/users/add", method="POST", data = {'user' : longstring, 'password' : 'password'} )
+        self.assertResponse(response, count=None, errCode = testLib.RestTestCase.ERR_BAD_USERNAME)
+        response = self.makeRequest("/users/add", method="POST", data = {'user' : 'user', 'password' : longstring} )
+        self.assertResponse(response, count=None, errCode = testLib.RestTestCase.ERR_BAD_PASSWORD)
+
+    
